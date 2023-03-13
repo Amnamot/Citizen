5c65e9a1eb5d443d64485483e58fb05367d0c8e4
Aimedin Chorobaev <eliothab58@gmail.com>
2023-03-13 13:37:56 +0600

    update data handlers

--- citizen-api/cmd/main.go
+++ citizen-api/cmd/main.go
@@ -18,12 +18,7 @@ func main() {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/v1/vices", handlers.GetVices).Methods("GET")
-	router.HandleFunc("/api/v1/characters", handlers.GetCharacters).Methods("GET")
-	router.HandleFunc("/api/v1/emotions", handlers.GetEmotions).Methods("GET")
-	router.HandleFunc("/api/v1/moralitys", handlers.GetMoralitys).Methods("GET")
-	router.HandleFunc("/api/v1/attitudes", handlers.GetAttitudes).Methods("GET")
-	router.HandleFunc("/api/v1/skills", handlers.GetSkills).Methods("GET")
+	router.HandleFunc("/api/v1/data", handlers.GetData).Methods("GET")
 
 	router.HandleFunc("/api/v1/deployNFT", handlers.DeployNFTItem).Methods("POST")
 	router.HandleFunc("/api/v1/editNFT", handlers.EditNFTItem).Methods("POST")
